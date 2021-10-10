954c238f28fe680c911c7ac164e48690e9502df3
Daniel Fernando Florez Cortes <dancortes90@gmail.com>
2021-10-10 15:07:39 -0500

    firebase config file

--- src/firebase.js
+++ src/firebase.js
@@ -7,14 +7,14 @@ import { getAuth, createUserWithEmailAndPassword, setPersistence, signInWithEmai
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  // apiKey: process.env.VUE_APP_API_KEY,
-  // authDomain: process.env.VUE_APP_AUTH_DOMAIN,
-  // databaseURL: process.env.VUE_APP_DATABASE_URL,
-  // projectId: process.env.VUE_APP_PROJECT_ID,
-  // storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
-  // messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
-  // appId: process.env.VUE_APP_APP_ID,
-  // measurementId: process.env.VUE_APP_MEASUREMENT_ID
+  apiKey: process.env.VUE_APP_API_KEY,
+  authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_DATABASE_URL,
+  projectId: process.env.VUE_APP_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_APP_ID,
+  measurementId: process.env.VUE_APP_MEASUREMENT_ID
 };
 
 // Initialize Firebase
